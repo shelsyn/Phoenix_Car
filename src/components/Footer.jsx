9839ba6e527565b022c6c70e2c2dc4e6be039523
shelsyn <shelsyn.guio@gmail.com>
2024-06-11 13:06:59 -0500

    feat: Actualizar enlaces en el componente Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,11 @@ import { AiOutlineWhatsApp, AiOutlineInstagram } from 'react-icons/ai';
 const navigation = {
   main: [
     { name: 'Inicio', href: '/' },
-  { name: 'Promociones', href: '#Promociones' },
-  { name: 'Crédito de vehículo', href: '/quienesSomos'},
-  { name: 'Crédito Hipotecario', href: '/hipoteca'},
-  { name: 'Catálogo Empresa', href: '/catalogoEmpresa' },
-  { name: 'Catálogo Persona Natural', href: '/catalogoNatural' }
+    { name: 'Promociones', href: '#Promociones' },
+    { name: 'Crédito de vehículo', href: '/quienesSomos'},
+    { name: 'Crédito Hipotecario', href: '/hipoteca'},
+    { name: 'Catálogo Empresa', href: '/catalogoEmpresa' },
+    { name: 'Catálogo Persona Natural', href: '/catalogoNatural' }
   ],
   social: [
     {
@@ -22,21 +22,22 @@ const navigation = {
     },
   ],
 };
+
 export default function Footer() {
   return (
     <>
       <footer className="bg-white">
-        <div className="mx-auto max-w-7xl overflow-hidden pt-20 px-6 sm:py-8 lg:px-8">
-          <nav className="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12 xs:flex xs:items-center xs:justify-center xs:space-x-4" aria-label="Footer">
+        <div className="mx-auto max-w-7xl overflow-hidden pt-10 px-4 sm:py-8 lg:px-8">
+          <nav className="flex flex-wrap justify-center sm:justify-between sm:space-x-12 xs:space-x-4" aria-label="Footer">
             {navigation.main.map((item) => (
-              <div key={item.name} className="pb-6">
+              <div key={item.name} className="mb-2 sm:mb-0">
                 <a href={item.href} className="text-sm leading-6 text-gray-600 hover:text-gray-900">
                   {item.name}
                 </a>
               </div>
             ))}
           </nav>
-          <div className="mt-10 flex justify-center space-x-10">
+          <div className="mt-6 flex justify-center space-x-6">
             {navigation.social.map((item) => (
               <a key={item.name} href={item.href} target="_blank" className="text-gray-400 hover:text-gray-500" rel="noreferrer">
                 <span className="sr-only">{item.name}</span>
@@ -44,7 +45,7 @@ export default function Footer() {
               </a>
             ))}
           </div>
-          <p className="mt-10 text-center text-xs leading-5 text-gray-500">&copy; 2023 Phoenix Car. Derechos reservados.</p>
+          <p className="mt-6 text-center text-xs leading-5 text-gray-500">&copy; 2023 Phoenix Car. Derechos reservados.</p>
         </div>
       </footer>
     </>
