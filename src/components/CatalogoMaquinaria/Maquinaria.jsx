6fb25ac9782a438c2da059f7836d8bf07ef4db5c
shelsyn <shelsyn.guio@gmail.com>
2025-03-05 17:13:20 -0500

    add industrial2

--- src/components/CatalogoMaquinaria/Maquinaria.jsx
+++ src/components/CatalogoMaquinaria/Maquinaria.jsx
@@ -14,8 +14,8 @@ export default function MaquinariaIndustrial() {
     { nombre: "Termoformadoras", img: "/termoformadora-3.jpg" },
     { nombre: "Líneas de llenado", img: "/LENEAS.jpg" },
     { nombre: "Compresores", img: "/Compresores.jpg" },
-    { nombre: "Tornos", img: "/tornos.jpg" },
-    { nombre: "Plantas eléctricas", img: "/placas.jpg" },
+    { nombre: "Tornos", img: "/ton.jpg" },
+    { nombre: "Plantas eléctricas", img: "/plam.jpg" },
   ];
 
   const beneficios = [
